6be6a4a56c6aa891c2d93dbb81022e58fff15ea3
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2024-06-11 12:59:58 +1200

    Add Contributiontechnique project

---
+++ Contributiontechnique/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> Array = new List<int> { 2, 5, 3 };
+
+        SubarrayContribution subarray = new SubarrayContribution();
+        int Count = subarray.FindSumOfSubarrays(Array);
+        Console.WriteLine(Count);
+    }
+}
+
+public class SubarrayContribution
+{
+    public int FindSumOfSubarrays(List<int> Array2)
+    {
+        int Sum = 0;
+        for (int i = 0; i < Array2.Count; i++)
+        {
+            Sum += Array2[i] * (i + 1) * (Array2.Count - i);
+        }
+        return Sum;
+    }
+}
