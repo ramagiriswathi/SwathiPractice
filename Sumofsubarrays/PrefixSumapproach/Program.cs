bb383c749ac4d7900e7ca1c1ca0dcd1b85581713
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2024-06-11 13:11:25 +1200

    Scalar Subarray code

---
+++ Sumofsubarrays/PrefixSumapproach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> SubarraySum = new List<int> { 2, 5, 3 };
+
+        SubarraySumCalculator subarraySumCalculator = new SubarraySumCalculator();
+        int count = subarraySumCalculator.FunctionSubarraySum(SubarraySum);
+        Console.WriteLine(count);
+    }
+}
+
+public class SubarraySumCalculator
+{int Ans = 0;
+    public int FunctionSubarraySum(List<int> array)
+    {
+        
+        for (int i = 0; i < array.Count; i++)
+        {
+             int sum = 0;
+            for (int j = i; j < array.Count; j++)
+            {
+                sum+=array[j];
+                Ans = sum+Ans; 
+            }
+        }
+        return Ans;
+    }
+}
+
---
+++ Sumofsubarrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> SubarraySum = new List<int> { 2, 5, 3 };
+
+        SubarraySumCalculator subarraySumCalculator = new SubarraySumCalculator();
+        int count = subarraySumCalculator.FunctionSubarraySum(SubarraySum);
+        Console.WriteLine(count);
+    }
+}
+
+public class SubarraySumCalculator
+{
+    public int FunctionSubarraySum(List<int> array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Count; i++)
+        {
+            for (int j = i; j < array.Count; j++)
+            {
+                for (int k = i; k <= j; k++)
+                {
+                    sum += array[k];
+                }
+            }
+        }
+        return sum;
+    }
+}
